d06677e835f29233c9336f7a0341ce9cfb079638
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-18 21:09:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,3 +3,19 @@ def prime?()
  if prime = [2,3,11]
    return TRUE
  end
+ 
+ def prime?(x)
+  if x == 1
+    return true 
+  elsif x < 1
+    return false
+  else
+  for y in 2..(x-1)
+    if (x % y) == 0
+      return false 
+    end
+  end 
+end
+ 
+  true
+end
